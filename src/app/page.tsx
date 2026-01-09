a5f24475baea5edc0a65b3977468cbbeb0df45f8
Arsh Verma <arshverma.dev@gmail.com>
2026-01-09 22:23:33 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/Hero';
+import PlatformSelector from '@/components/PlatformSelector';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <PlatformSelector />
+    </main>
+  );
+}
