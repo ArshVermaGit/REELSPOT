d040dc36f57c7696f2ea9da71ca017db495c90f7
Arsh Verma <arshverma.dev@gmail.com>
2026-01-10 00:29:53 +0530

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { API_CONFIG } from "@/config/api";
+import { prisma } from "@/lib/prisma";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: API_CONFIG.GOOGLE_CLIENT_ID,
@@ -10,16 +13,25 @@ const handler = NextAuth({
     }),
   ],
   secret: API_CONFIG.nextAuthSecret,
+  session: {
+    strategy: "jwt",
+  },
   pages: {
     signIn: '/',
   },
   callbacks: {
     async session({ session, token }) {
-      if (session?.user) {
+      if (session?.user && token.sub) {
         // @ts-expect-error session.user.id is not indexable
         session.user.id = token.sub;
       }
       return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
     },
   },
 });
