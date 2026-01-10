5ee4f7876b6598d1624312994273310e1827c04c
Arsh Verma <arshverma.dev@gmail.com>
2026-01-11 01:08:43 +0530

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col bg-zinc-50/50">
             <Navbar />
-            <main className="flex-grow pt-16">
+            <main className="flex-grow pt-20 animate-fade-in">
                 {children}
             </main>
             <Footer />
