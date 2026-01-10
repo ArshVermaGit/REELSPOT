6e435a19834b5912e83cb6e94e9f52ccd35adc09
Arsh Verma <arshverma.dev@gmail.com>
2026-01-11 01:08:32 +0530

    Update FloatingIcons.jsx

--- src/components/home/FloatingIcons.jsx
+++ src/components/home/FloatingIcons.jsx
@@ -4,32 +4,44 @@ import { Instagram, Youtube, Facebook, Music2 } from 'lucide-react';
 const FloatingIcons = () => {
     return (
         <div className="absolute inset-0 pointer-events-none overflow-hidden select-none z-0">
-            {/* Instagram - Top Left */}
-            <div className="absolute top-[15%] left-[10%] opacity-10 animate-float">
-                <div className="bg-gradient-to-tr from-purple-500 via-pink-500 to-orange-500 p-4 rounded-3xl rotate-[-12deg] shadow-2xl">
-                    <Instagram size={64} color="white" strokeWidth={1.5} />
-                </div>
+            {/* 1. Instagram - Top Left */}
+            <div className="absolute top-[10%] left-[8%] opacity-30 animate-float-1 animate-pulse-slow">
+                <Instagram size={48} className="text-black rotate-[-12deg]" strokeWidth={1.5} />
             </div>
 
-            {/* YouTube - Top Right */}
-            <div className="absolute top-[20%] right-[12%] opacity-10 animate-float-reverse delay-100">
-                 <div className="bg-red-600 p-4 rounded-3xl rotate-[15deg] shadow-2xl">
-                    <Youtube size={72} color="white" strokeWidth={1.5} />
-                </div>
+            {/* 2. YouTube - Top Right */}
+            <div className="absolute top-[15%] right-[10%] opacity-25 animate-float-2 animate-pulse-slow delay-300">
+                <Youtube size={48} className="text-black rotate-[12deg]" strokeWidth={1.5} />
             </div>
 
-            {/* TikTok/Music - Bottom Left */}
-            <div className="absolute bottom-[20%] left-[15%] opacity-10 animate-float-reverse delay-200">
-                <div className="bg-black p-4 rounded-3xl rotate-[8deg] shadow-2xl">
-                    <Music2 size={56} color="white" strokeWidth={1.5} />
-                </div>
+            {/* 3. Facebook - Bottom Left */}
+            <div className="absolute bottom-[20%] left-[12%] opacity-20 animate-float-1 animate-pulse-slow delay-700">
+                <Facebook size={48} className="text-black rotate-[-8deg]" strokeWidth={1.5} />
             </div>
 
-            {/* Facebook - Bottom Right */}
-            <div className="absolute bottom-[25%] right-[8%] opacity-10 animate-float delay-300">
-                <div className="bg-blue-600 p-4 rounded-3xl rotate-[-10deg] shadow-2xl">
-                    <Facebook size={60} color="white" strokeWidth={1.5} />
-                </div>
+            {/* 4. TikTok - Bottom Right */}
+            <div className="absolute bottom-[18%] right-[15%] opacity-25 animate-float-2 animate-pulse-slow delay-500">
+                 <Music2 size={48} className="text-black rotate-[10deg]" strokeWidth={1.5} />
+            </div>
+
+            {/* 5. Instagram - Center Right (Offset) */}
+            <div className="absolute top-[45%] right-[5%] opacity-20 animate-float-1 animate-pulse-slow delay-200">
+                <Instagram size={48} className="text-black rotate-[15deg]" strokeWidth={1.5} />
+            </div>
+
+             {/* 6. YouTube - Bottom Center Left */}
+             <div className="absolute bottom-[35%] left-[5%] opacity-15 animate-float-2 animate-pulse-slow delay-1000">
+                <Youtube size={48} className="text-black rotate-[-5deg]" strokeWidth={1.5} />
+            </div>
+
+            {/* 7. Facebook - Top Center Left */}
+            <div className="absolute top-[25%] left-[25%] opacity-10 animate-float-1 animate-pulse-slow delay-150">
+                <Facebook size={48} className="text-black rotate-[-15deg]" strokeWidth={1.5} />
+            </div>
+
+            {/* 8. TikTok - Top Center Right */}
+            <div className="absolute top-[30%] right-[30%] opacity-10 animate-float-2 animate-pulse-slow delay-400">
+                <Music2 size={48} className="text-black rotate-[8deg]" strokeWidth={1.5} />
             </div>
         </div>
     );
