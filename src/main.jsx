65eede2227ad83d16d5b07d5dff8ebb4fbef4f7b
Arsh Verma <arshverma.dev@gmail.com>
2026-01-11 15:28:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
+console.log('MAIN.JSX EXECUTING');
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary'
-import './index.css'
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+console.log('Imports done');
+
+try {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) throw new Error("Root element not found");
+
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>,
+  )
+  console.log('Root render called');
+} catch (e) {
+  console.error('CRASH IN MAIN.JSX:', e);
+}
