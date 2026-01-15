ad2425b3747bd58179270ca2387185dc5ac01eb4
Arsh Verma <arshverma.dev@gmail.com>
2026-01-16 01:00:45 +0530

    Create facebook.service.js

---
+++ src/services/platforms/facebook.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { extractMediaId, PLATFORMS } from '../platformDetector.js';
+
+export const fetchFacebookData = async (url, apiKey) => {
+    const MediaError = (await import('../download.service.js')).MediaError;
+    
+    const videoId = extractMediaId(url, PLATFORMS.FACEBOOK);
+    if (!videoId) throw new MediaError('Could not extract Facebook Video ID', 'INVALID_ID');
+
+    if (!apiKey) throw new MediaError('Facebook Access Token required', 'AUTH_REQUIRED');
+
+    try {
+        const endpoint = `https://graph.facebook.com/v18.0/${videoId}?fields=source,title,description,picture&access_token=${apiKey}`;
+        const response = await axios.get(endpoint);
+        const data = response.data;
+
+        return {
+            title: data.title || data.description || 'Facebook Video',
+            thumbnail: data.picture,
+            author: 'Facebook User',
+            formats: [
+                { type: 'video', quality: 'SD', ext: 'mp4', url: data.source }
+            ]
+        };
+    } catch (e) {
+        throw new MediaError('Facebook API Error', 'API_ERROR');
+    }
+};
