fd343f9f56ee964e63f0cd99a321d508c9582ae9
Arsh Verma <arshverma.dev@gmail.com>
2026-01-16 01:25:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary'
+import ErrorBoundary from './components/shared/ErrorBoundary'
 import './styles/index.css'
 
 console.log('Imports done');
