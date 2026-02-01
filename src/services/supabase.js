54221ec5f4ead2d9d4a194887c3d4902ce639620
Arsh Verma <arshverma.dev@gmail.com>
2026-02-02 01:48:02 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import config from '../config'
+import config from '../config/index.js'
 
 const supabaseUrl = config.supabase.url
 const supabaseAnonKey = config.supabase.anonKey
